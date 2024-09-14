54a70a9264a070fed416cb87e625ced42615b22a
PaddyZ <grassman0209@gmail.com>
2024-09-14 13:05:06 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from src.weatherTSF import logger
+
+import tensorflow as tf
+from src.weatherTSF.components.data_injection import getDataset
+from src.weatherTSF.components.pretrian_model import cleanUpOutlier, splitDataAndNormalization
+from src.weatherTSF.components.data_windowing import WindowGenerator,split_window,make_dataset
+from src.weatherTSF.components.single_step_models import compile_and_fit
+
+STAGE_NAME ="DATA_INJECTION"
+STAGE_NAME_ONE = "PRETRAIN_MODEL"
+STAGE_NAME_TWO = "DATA_WINDOWING"
+STAGE_NAME_THREE = "SINGLE_STEP_MODELS"
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        
+        df, date_time = getDataset()
+        print(df.head())
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+        logger.info(f">>>>>> stage {STAGE_NAME_ONE} started <<<<<<")
+        column_indices = {name: i for i, name in enumerate(df.columns)}
+        cleanUpOutlier(df)
+        train_df,val_df,test_df,num_features = splitDataAndNormalization(df)
+        logger.info(f">>>>>> stage {STAGE_NAME_ONE} completed <<<<<<\n\nx==========x")
+
+        logger.info(f">>>>>> stage {STAGE_NAME_TWO} started <<<<<<")
+        WindowGenerator.split_window = split_window
+        WindowGenerator.make_dataset = make_dataset
+        wide_window = WindowGenerator(
+        input_width=24, label_width=24, shift=1,
+        train_df=train_df,val_df=val_df,test_df=test_df,
+        label_columns=['T (degC)'])
+        logger.info(f">>>>>> stage {STAGE_NAME_TWO} completed <<<<<<\n\nx==========x")
+
+        logger.info(f">>>>>> stage {STAGE_NAME_THREE} started <<<<<<")
+        lstm_model = tf.keras.models.Sequential([
+            # Shape [batch, time, features] => [batch, time, lstm_units]
+            tf.keras.layers.LSTM(32, return_sequences=True),
+            # Shape => [batch, time, features]
+            tf.keras.layers.Dense(units=1)
+        ])
+        history = compile_and_fit(lstm_model, wide_window)
+        lstm_model.save("./src/weatherTSF/models/lstm")
+        logger.info(f">>>>>> stage {STAGE_NAME_THREE} completed <<<<<<\n\nx==========x")
+        
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
---
+++ src/weatherTSF/components/pretrian_model.py
@@ -0,0 +1,28 @@
+
+
+def splitDataAndNormalization(df):
+    
+    n = len(df)
+    train_df = df[0:int(n*0.7)]
+    val_df = df[int(n*0.7):int(n*0.9)]
+    test_df = df[int(n*0.9):]
+    num_features = df.shape[1]
+    train_mean = train_df.mean()
+    train_std = train_df.std()
+
+    train_df = (train_df - train_mean) / train_std
+    val_df = (val_df - train_mean) / train_std #validation_df
+    test_df = (test_df - train_mean) / train_std
+    return train_df,val_df,test_df,num_features
+
+def cleanUpOutlier(df):
+    wv = df['wv (m/s)']
+    bad_wv = wv == -9999.0
+    wv[bad_wv] = 0.0
+
+    max_wv = df['max. wv (m/s)']
+    bad_max_wv = max_wv == -9999.0
+    max_wv[bad_max_wv] = 0.0
+
+
+    
